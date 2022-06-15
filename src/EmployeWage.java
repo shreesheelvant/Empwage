249700e2037c579ec21491448cbcee7d88625d77
unknown <suntanabhagyashree@gmail.com>
2022-06-15 18:23:50 +0530

    ADDED: add part time

--- src/EmployeWage.java
+++ src/EmployeWage.java
@@ -1,16 +1,21 @@
 public class EmployeWage {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == IS_FULL_TIME)
+            empHrs = 4;
+        else if (empCheck == IS_FULL_TIME)
             empHrs = 8;
         else
             empHrs = 0;
         empWage = empHrs * EMP_RATE_PER_HOUR;
-            System.out.println("Emp Wage: " + empWage);
+        System.out.println("Emp Wage: " + empWage);
+    }
+}
 
     }
 
